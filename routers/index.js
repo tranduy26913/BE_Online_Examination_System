753df8a8fe88dd1b75c6777f44c9df8238d70d90
tranduy26913 <57041565+tranduy26913@users.noreply.github.com>
2022-11-05 20:20:10 +0700

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -5,7 +5,7 @@ const CourseRoutes = require('./CourseRoutes');
 const BillRoute = require('./BillRoute');
 const ExamRoutes = require("./ExamRoutes");
 const QuestionRoutes = require("./QuestionRoutes")
-const TakeExamRoutes = require("./takeExamRoutes")
+const TakeExamRoutes = require("./TakeExamRoutes")
 const QuestionBankRoutes = require("./QuestionBankRoutes")  
 const StatisticRoutes = require("./StatisticRoutes")  
 
